985af4b8d53f370c324afb9b750a0f5840d71f8d
Allan <106794039+allanp2m@users.noreply.github.com>
2025-05-12 17:57:27 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/diagnose', async (req, res) => {
+  try {
+    const response = await axios.post('http://localhost:5000/predict', req.body);
+    res.json(response.data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Node.js server running on http://localhost:3000');
+});
